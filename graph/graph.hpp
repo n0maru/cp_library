18bd08106578ba604321d97be5a4c56b1b2146cd
Ryuma Noma <noma11.develop@gmail.com>
2024-12-25 01:22:30 +0900

    無向グラフへの変換

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -10,3 +10,17 @@ ostream& operator <<(ostream& out, const edge& e) {
     out << '[' << e.to << ' ' << e.cost << ' ' << e.id << ']';
     return out;
 }
+
+// 未テスト
+// 重み無しグラフに変換
+// O(V+E)
+vector<vector<int>> convert_uw(const graph& g) {
+    int n = g.size();
+    vector<vector<int>> res(n);
+    for (int i = 0; i < n; ++i) {
+        for (auto&& e : g[i]) {
+            res[i].push_back(e.to);
+        }
+    }
+    return res;
+}
