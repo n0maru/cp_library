d7626b87200e62f9b638713e37bdd4df61b38542
Ryuma Noma <noma11.develop@gmail.com>
2025-01-01 02:32:00 +0900

    fix kbfs

--- graph/kbfs.hpp
+++ graph/kbfs.hpp
@@ -2,29 +2,28 @@
 #include "../other/const.hpp"
 #include "graph.hpp"
 
-// test: https://atcoder.jp/contests/arc005/submissions/61071912
+// test: https://atcoder.jp/contests/arc005/submissions/61285177
 // O( k(V+E) )
-template <class T>
-vector<T> kbfs(const graph<T>& g, int start, int k) {
+vector<int> kbfs(const gint& g, int start, int k) {
     int n = g.size();
     deque<queue<int>> qs(k);
-    vector<ll> dis(n, inf<T>);
-    vector<bool> vis(n);
+    vector<int> dis(n, inf<int>);
 
     int item_num = 1;
+    int nowd = 0;
     qs[0].emplace(start);
     dis[start] = 0;
 
     while (item_num > 0) {
-        while (qs[0].empty) {
+        while (qs[0].empty()) {
             qs.pop_front();
             qs.emplace_back();
+            ++nowd;
         }
         int pos = qs[0].front();
         qs[0].pop();
         --item_num;
-        if (vis[pos]) continue;
-        vis[pos] = true;
+        if (nowd > dis[pos]) continue;
 
         for (auto&& e : g[pos]) {
             if (dis[e.to] > dis[pos] + e.cost) {
